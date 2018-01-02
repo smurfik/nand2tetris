20cdbbf98cd2813a1c8ef9cfbb3acc1b5049a3de
smurfik <tammy@mlpinit.com>
2018-01-01 22:19:11 -0600

    add command_type methods

--- projects/07/vm_translator/parser.rb
+++ projects/07/vm_translator/parser.rb
@@ -1,8 +1,27 @@
 class Parser
   attr_reader :lines
+  C_PUSH = Object.new
+  C_ARITHMETIC = Object.new
 
   def initialize(contents)
     @lines = split_lines(contents)
+  end
+
+  def has_more_commands?
+    lines.any?
+  end
+
+  def advance
+    @current_command = lines.shift
+  end
+
+  def command_type
+    case @current_command.split(' ').first
+    when 'push'
+      C_PUSH
+    else
+      C_ARITHMETIC
+    end
   end
 
   def split_lines(contents)
--- projects/07/vm_translator/vm_translator.rb
+++ projects/07/vm_translator/vm_translator.rb
@@ -6,7 +6,15 @@ module VmTranslator
     file_name = ARGV[0]
     file_contents = ARGF.read
     parser = Parser.new(file_contents)
-    puts parser.lines.inspect
+    while parser.has_more_commands?
+      parser.advance
+      case parser.command_type
+      when Parser::C_ARITHMETIC
+        puts "arithmetic"
+      when Parser::C_PUSH
+        puts "push"
+      end
+    end
   end
 end
 
