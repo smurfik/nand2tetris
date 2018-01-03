950432691b35e6934a40527b1c46c6b5a2a8da44
smurfik <tammy@mlpinit.com>
2018-01-02 20:01:19 -0600

    add arg1 and arg2 to parser

--- projects/07/vm_translator/parser.rb
+++ projects/07/vm_translator/parser.rb
@@ -1,7 +1,14 @@
 class Parser
   attr_reader :lines
-  C_PUSH = Object.new
   C_ARITHMETIC = Object.new
+  C_PUSH = Object.new
+  C_POP = Object.new
+  C_LABEL = Object.new
+  C_GOTO = Object.new
+  C_IF = Object.new
+  C_FUNCTION = Object.new
+  C_RETURN = Object.new
+  C_CALL = Object.new
 
   def initialize(contents)
     @lines = split_lines(contents)
@@ -19,9 +26,36 @@ class Parser
     case @current_command.split(' ').first
     when 'push'
       C_PUSH
+    when 'pop'
+      C_POP
+    when 'label'
+      C_LABEL
+    when 'goto'
+      C_GOTO
+    when 'if-goto'
+      C_IF
+    when 'function'
+      C_FUNCTION
+    when 'call'
+      C_CALL
+    when 'return'
+      C_RETURN
     else
       C_ARITHMETIC
     end
+  end
+
+  def arg1
+    case command_type
+    when C_ARITHMETIC
+      @current_command
+    else
+      @current_command.split[1]
+    end
+  end
+
+  def arg2
+    @current_command.split[2].to_i
   end
 
   def split_lines(contents)
